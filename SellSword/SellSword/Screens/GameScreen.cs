c24a91a5672b21ae058207ab5f9fba646ec331f3
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-10 17:32:29 -0500

    Game Screen added, managed by Screenmanager

---
+++ SellSword/SellSword/Screens/GameScreen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+using SellSword.Gameplay;
+using SellSword.Managers;
+
+namespace SellSword.Screens
+{
+    public class GameScreen : Screen
+    {
+        //Handle to the gameplay object. Currently the game screen will call the gameplay object to display the world onto the screen.
+        //Screen class used to abstract away screen logic from game logic.
+        private SellSwordGame m_game;
+
+        public GameScreen()
+        {
+            m_game = new SellSwordGame(this);
+        }
+
+        public override void UnloadContent()
+        {
+        }
+
+        //Loads necessary game starting content
+        public override void LoadContent( ContentManager content)
+        {
+            m_game.LoadContent(content);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            m_game.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            m_game.Draw(spriteBatch);
+        }
+
+        //Once game is running, the three ways out are, exit, pause, and lose the game. Pause and losing state changes will be called from here
+        public void PauseGame()
+        {
+            ScreenManager.Instance.ChangeState(ScreenManager.GameState.Pause);
+        }
+
+        public void GameOver()
+        {
+            ScreenManager.Instance.ChangeState(ScreenManager.GameState.GameOver);
+        }
+    }
+}
---
+++ SellSword/SellSword/Screens/Screen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace SellSword.Screens
+{
+    public class Screen
+    {
+        public Screen() { }
+
+        public virtual void LoadContent( ContentManager content) { }
+
+        public virtual void UnloadContent() { }
+
+        public virtual void Update(GameTime gameTime) { }
+
+        public virtual void Draw(SpriteBatch spriteBatch) { }
+    }
+}
