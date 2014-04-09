6086532d6d4a6aab30341a42eef86a6dd7afd4a5
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-04-08 22:57:10 -0400

    Created Collision Manager

---
+++ SellSword/SellSword/Gameplay/Collision/CollisionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SellSword.Gameplay.Collision
+{
+    public class CollisionManager
+    {
+    }
+}
